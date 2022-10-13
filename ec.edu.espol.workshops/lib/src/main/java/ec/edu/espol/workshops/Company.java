e39531da84dca074d9f9bcc56378ebbaaf85a699
pedbvasq <pedbvasq@espol.edu.ec>
2022-10-13 16:35:10 -0500

    Creation of Class CarInsurance

---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Company.java
@@ -0,0 +1,9 @@
+package ec.edu.espol.workshops;
+
+public class Company {
+
+	public Company() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
@@ -0,0 +1,9 @@
+package ec.edu.espol.workshops;
+
+public class Customer {
+
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
