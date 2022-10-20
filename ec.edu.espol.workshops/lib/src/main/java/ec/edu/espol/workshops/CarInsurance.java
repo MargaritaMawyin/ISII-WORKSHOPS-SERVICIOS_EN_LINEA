1827d52f5a40ab93e4260106f8e8620432262130
MargaritaMawyin <margomawyin@hotmail.es>
2022-10-20 16:10:42 -0500

    checkstyle specialist PMD programmer Margarita Mawyin

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -17,18 +17,12 @@ public class CarInsurance {
 		char gender = res == 0 ? 'M': 'F';
 		System.out.println("What is your civil state? \n 0)single \n 1)married ");
 		boolean married = sc.nextInt()==1;
-		
 		Customer customer2 = new Customer(age, gender, married) ;
 		verify_business_rules(customer2);
-		
 		System.out.println(verify_business_rules(customer2));
-
 	}
-	
 	public static int verify_business_rules(Customer customer) {
-		
 		int premium_car = 500;
-		
 		if (validatePolitics(customer)) {
 			
 			if (!customer.isMarried() && customer.isMale() && customer.ageLessThan(25)) {
@@ -40,17 +34,11 @@ public class CarInsurance {
 			else if(customer.ageMoreThan(45) && customer.ageLessThan(65)){
 				premium_car -= 100;
 			}
-			
 			return premium_car;
-			
 		}
 		else {
 			return -1;
 		}
-		
-		
-		
-		
 	}
 	
 	public static boolean validatePolitics(Customer customer) {
