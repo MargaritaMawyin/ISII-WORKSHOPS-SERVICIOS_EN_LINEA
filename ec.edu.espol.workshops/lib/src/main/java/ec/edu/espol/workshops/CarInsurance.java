0cb487b680659542a31a9fc5de6cec201e9a530c
KevinChevez <kchevez@espol.edu.ec>
2022-10-21 09:38:39 -0500

    Checkstyle problems solved.

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -2,49 +2,72 @@ package ec.edu.espol.workshops;
 
 import java.util.Scanner;
 
-
+/**
+ * Summary of JavaDoc will be here.
+ *
+ * @author KevinChevez, MargaritaMawyin, PedroBajana
+ */
 public class CarInsurance {
-	
-	public static int BASE_PREMIUM = 500; 
 
-	public static void main(String[] args) {
-		Customer customer = new Customer();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Type your age");
-		int age = sc.nextInt();
-		System.out.println("Type your gender \n 0)Male \n 1)Female");
-		int res = sc.nextInt();
-		char gender = res == 0 ? 'M': 'F';
-		System.out.println("What is your civil state? \n 0)single \n 1)married ");
-		boolean married = sc.nextInt()==1;
-		Customer customer2 = new Customer(age, gender, married) ;
-		verify_business_rules(customer2);
-		System.out.println(verify_business_rules(customer2));
-	}
-	public static int verify_business_rules(Customer customer) {
-		int premium_car = 500;
-		if (validatePolitics(customer)) {
-			
-			if (!customer.isMarried() && customer.isMale() && customer.ageLessThan(25)) {
-				premium_car += 1500;
-			}
-			else if (!customer.isMale() || customer.isMarried()) {
-				premium_car -= 200;
-			} 
-			else if(customer.ageMoreThan(45) && customer.ageLessThan(65)){
-				premium_car -= 100;
-			}
-			return premium_car;
-		}
-		else {
-			return -1;
-		}
-	}
-	
-	public static boolean validatePolitics(Customer customer) {
-		 return customer.ageLessThan(80);
-	}
-	
-	
+  /**
+   * Static variable that contains the base of premium.
+   */
+  public static int BASE_PREMIUM = 500;
+
+  /**
+   * Main of the application, this main will be run.
+   *
+   * @param args Receive the arguments but this won't used.
+   */
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Type your age");
+    int age = sc.nextInt();
+    System.out.println("Type your gender \n 0)Male \n 1)Female");
+    int res = sc.nextInt();
+    char gender = res == 0 ? 'M' : 'F';
+    System.out.println("What is your civil state? \n 0)single \n 1)married ");
+    boolean married = sc.nextInt() == 1;
+    Customer customer = new Customer(age, gender, married);
+    verify_business_rules(customer);
+    System.out.println(verify_business_rules(customer));
+
+    sc.close();
+  }
+
+  /**
+   * Static function which verify the rules of the business.
+   *
+   * @param customer Receive a Customer object to verify if the customer complies with the rules.
+   * @return An integer with the amount of premium or -1 in error.
+   */
+  public static int verify_business_rules(Customer customer) {
+    int premiumCar = 500;
+    if (validatePolitics(customer)) {
+
+      if (!customer.isMarried() && customer.isMale() && customer.ageLessThan(25)) {
+        premiumCar += 1500;
+      } else if (!customer.isMale() || customer.isMarried()) {
+        premiumCar -= 200;
+      } else if (customer.ageMoreThan(45) && customer.ageLessThan(65)) {
+        premiumCar -= 100;
+      }
+      return premiumCar;
+    } else {
+      return -1;
+    }
+  }
+
+  /**
+   * Static function to validate if the Customer given as parameter complies with
+   * the Politics of the company.
+   *
+   * @param customer The Customer object which will be validated.
+   * @return True if it's OK, or false if the customer don't complies with the
+   *         rules of the company.
+   */
+  public static boolean validatePolitics(Customer customer) {
+    return customer.ageLessThan(80);
+  }
 
 }
--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
@@ -1,58 +1,77 @@
 package ec.edu.espol.workshops;
 
-import javax.naming.directory.NoSuchAttributeException;
-
+/**
+ * Summary of JavaDoc will be here.
+ *
+ * @author KevinChevez, MargaritaMawyin, PedroBajana
+ */
 public class Customer {
-	private int age;
-	private char gender;
-	private boolean stateCivil;
-	
-	public Customer() {
-		this.age = 18;
-		this.gender = 'M';
-		this.stateCivil = false;
-	}
-	public Customer(int age, char gender, boolean stateCivil) {
-		this.age = age;
-		this.gender = gender;
-		this.stateCivil = stateCivil;
-	}
+  private int age;
+  private char gender;
+  private boolean stateCivil;
 
+  /**
+   * Summary of JavaDoc will be here.
+   */
+  public Customer() {
+    this.age = 18;
+    this.gender = 'M';
+    this.stateCivil = false;
+  }
 
-	//Getters by Kevin
-	public int getAge() {
-		return this.age;
-	}
-	public char getGender() {
-		return this.gender ;
-	}
-	public boolean getStateCivil() {
-		return this.stateCivil;
-	}
-	
-	//Setters by Margarita
-	public void setAge(int age){
-	   this.age=age;
-	}
-	public void setGender(char gender){
-	   this.gender=gender;
-	}
-	public void setStateCivil(boolean stateCivil){
-	   this.stateCivil=stateCivil;
-	}
-    //Functions for Customer
-	public boolean isMarried() {
-		return this.getStateCivil();
-	}
-	
-	public boolean isMale() {
-		return (this.gender == 'M');
-	}
-	
-	public boolean ageLessThan(int age) {
-		return (this.age < age);
-	}
-	public boolean ageMoreThan(int age) {
-		return (this.age > age);
-	}
+  /**
+   * Summary of JavaDoc will be here.
+   *
+   * @param age Description will be here.
+   * @param gender Description will be here.
+   * @param stateCivil Description will be here.
+   */
+  public Customer(int age, char gender, boolean stateCivil) {
+    this.age = age;
+    this.gender = gender;
+    this.stateCivil = stateCivil;
+  }
+
+  // Getters by Kevin
+  public int getAge() {
+    return this.age;
+  }
+
+  public char getGender() {
+    return this.gender;
+  }
+
+  public boolean getStateCivil() {
+    return this.stateCivil;
+  }
+
+  // Setters by Margarita
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void setGender(char gender) {
+    this.gender = gender;
+  }
+
+  public void setStateCivil(boolean stateCivil) {
+    this.stateCivil = stateCivil;
+  }
+
+  // Functions for Customer
+  public boolean isMarried() {
+    return this.getStateCivil();
+  }
+
+  public boolean isMale() {
+    return (this.gender == 'M');
+  }
+
+  public boolean ageLessThan(int age) {
+    return (this.age < age);
+  }
+
+  public boolean ageMoreThan(int age) {
+    return (this.age > age);
+  }
 }
