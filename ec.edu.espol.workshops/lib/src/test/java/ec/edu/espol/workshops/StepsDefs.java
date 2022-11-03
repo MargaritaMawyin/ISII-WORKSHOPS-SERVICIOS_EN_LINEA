046f821bde6c9e3d6035b25d27b508a6eafd8d74
KevinChevez <kchevez@espol.edu.ec>
2022-11-02 20:50:33 -0500

    Uploading tests

--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
@@ -1,27 +1,34 @@
 package ec.edu.espol.workshops;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import static org.junit.Assert.*;
+
 public class StepsDefs {
- private String today;
- private String actualAnswer;
- 
- @Given("today is Sunday")
- public void today_is_Sunday() {
- today = "Sunday";
- }
- 
- @Given("today is {string}")
- public void today_is(String today) {
- this.today = today;
- }
- 
- @When("I ask whether it's Friday yet")
- public void i_ask_whether_it_s_Friday_yet() {
- actualAnswer = IsItFriday.isItFriday(today);
- }
- 
- @Then("I should be told {string}")
- public void i_should_be_told(String expectedAnswer) {
- assertEquals(expectedAnswer, actualAnswer);
- } 
- 
+  private Customer customer;
+  private String actualAnswer;
+
+
+  @Given("the customer is a {string} years old {string} {string}")
+  public void customer_is(String age, String civilState, String gender) {
+    char charGender = gender.compareTo("male") == 0 ? 'M' : 'F';
+    boolean isMarried = civilState.compareTo("married") == 0;
+    this.customer = new Customer(Integer.parseInt(age), charGender, isMarried);
+  }
+
+  @When("calculate the insurance prime and ask if is 500")
+  public void ask_if_insurance_prime_is500() {
+    int insurancePrime = this.calculate_the_insurance_prime();
+    this.actualAnswer = insurancePrime == 500 ? "Yes" : "No";
+  }
+
+  @Then("the answer should be {string}")
+  public void answer_should_be(String expectedAnswer) {
+    assertEquals(expectedAnswer, actualAnswer);
+  }
+  
+  private int calculate_the_insurance_prime() {
+    return CarInsurance.verify_business_rules(this.customer);
+  }
 }
